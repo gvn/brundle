d97790a520e9a50f9f674b54d0115d49272b9c42
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-02-28 23:03:40 -0800

    fixing diff logic

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var lwip = require('lwip');
 
 const PRESERVE_MATCHES = false;
 
-lwip.open('img/slicer-gvn-1.png', function(err, image1){
-  lwip.open('img/slicer-gvn-2.png', function (err, image2) {
+lwip.open('img/dude-90.jpg', function(err, image1){
+  lwip.open('img/dude-100.jpg', function (err, image2) {
     lwip.create(image1.width(), image1.height(), 'white', function(err, diffImage) {
       var pixel1, pixel2, savedPixel;
 
@@ -17,10 +17,16 @@ lwip.open('img/slicer-gvn-1.png', function(err, image1){
           pixel1 = image1.getPixel(x,y);
           pixel2 = image2.getPixel(x,y);
 
-          if (pixel1.r !== pixel2.r &&
-              pixel1.g !== pixel2.g &&
-              pixel1.b !== pixel2.b) {
+          if (pixel1.r === pixel2.r &&
+              pixel1.g === pixel2.g &&
+              pixel1.b === pixel2.b) {
 
+             if (PRESERVE_MATCHES) {
+              batch.setPixel(x, y, pixel1);
+            }
+
+            savedPixel = pixel1;
+          } else {
             // Difference
             // batch.setPixel(x, y, {
             //   r: Math.abs(pixel2.r - pixel1.r),
@@ -35,15 +41,12 @@ lwip.open('img/slicer-gvn-1.png', function(err, image1){
             //   b: Math.floor((pixel2.b + pixel1.b) / 2)
             // });
 
-            // Saved pixel
-            savedPixel.a = savedPixel.a > 0 ? Math.floor(savedPixel.a - 1) : 0; // Fade out
-            batch.setPixel(x, y, savedPixel);
-          } else {
-             if (PRESERVE_MATCHES) {
-              batch.setPixel(x, y, pixel1);
-            }
+            // Solid color
+            batch.setPixel(x, y, 'black');
 
-            savedPixel = pixel1;
+            // Saved pixel
+            // savedPixel.a = savedPixel.a > 0 ? Math.floor(savedPixel.a - 1) : 0; // Fade out
+            // batch.setPixel(x, y, savedPixel);
           }
         }
         console.log('Finished row ' + (y + 1) + ' of ' + image1.height());
