463dc6ae0ae3a8f14fc803812dd672129c4fab75
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-02-28 22:48:25 -0800

    adding preserve match option; various diff methods

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var lwip = require('lwip');
 
-lwip.open('img/statue-0.jpg', function(err, image){
-  lwip.open('img/statue-100.jpg', function (err, image2) {
-    lwip.create(image.width(), image.height(), 'white', function(err, diffImage) {
-      var pixel1, pixel2;
+const PRESERVE_MATCHES = false;
+
+lwip.open('img/slicer-gvn-1.png', function(err, image1){
+  lwip.open('img/slicer-gvn-2.png', function (err, image2) {
+    lwip.create(image1.width(), image1.height(), 'white', function(err, diffImage) {
+      var pixel1, pixel2, savedPixel;
 
       var batch = diffImage.batch();
 
-      for (var y = 0; y < image.height(); y++) {
-        for (var x = 0; x < image.width(); x++) {
+      savedPixel = image1.getPixel(0,0);
 
-          pixel1 = image.getPixel(x,y);
+      for (var y = 0; y < image1.height(); y++) {
+        for (var x = image1.width() - 1; x > 0; x--) {
+
+          pixel1 = image1.getPixel(x,y);
           pixel2 = image2.getPixel(x,y);
 
           if (pixel1.r !== pixel2.r &&
@@ -25,14 +29,24 @@ lwip.open('img/statue-0.jpg', function(err, image){
             // });
 
             // Average
-            batch.setPixel(x, y, {
-              r: Math.floor((pixel2.r + pixel1.r) / 2),
-              g: Math.floor((pixel2.g + pixel1.g) / 2),
-              b: Math.floor((pixel2.b + pixel1.b) / 2)
-            });
+            // batch.setPixel(x, y, {
+            //   r: Math.floor((pixel2.r + pixel1.r) / 2),
+            //   g: Math.floor((pixel2.g + pixel1.g) / 2),
+            //   b: Math.floor((pixel2.b + pixel1.b) / 2)
+            // });
+
+            // Saved pixel
+            savedPixel.a = savedPixel.a > 0 ? Math.floor(savedPixel.a - 1) : 0; // Fade out
+            batch.setPixel(x, y, savedPixel);
+          } else {
+             if (PRESERVE_MATCHES) {
+              batch.setPixel(x, y, pixel1);
+            }
+
+            savedPixel = pixel1;
           }
         }
-        console.log('finished row: ' + y);
+        console.log('Finished row ' + (y + 1) + ' of ' + image1.height());
       }
 
       batch.writeFile('img/diff.png', function (err) {
