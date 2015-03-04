6d748a69a023e366b25b044758ee8bf7d8ae3b44
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-03-03 21:17:47 -0800

    vizmode as option

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var lwip = require('lwip');
 
 const PRESERVE_MATCHES = false;
+const VIZMODE = 'average';
 
-lwip.open('img/dude-90.jpg', function(err, image1){
-  lwip.open('img/dude-100.jpg', function (err, image2) {
+lwip.open('img/slicer-gvn-1.png', function(err, image1){
+  lwip.open('img/slicer-gvn-2.png', function (err, image2) {
     lwip.create(image1.width(), image1.height(), 'white', function(err, diffImage) {
-      var pixel1, pixel2, savedPixel;
-
-      var batch = diffImage.batch();
-
-      savedPixel = image1.getPixel(0,0);
+      var
+        batch = diffImage.batch(),
+        pixel1,
+        pixel2,
+        savedPixel = image1.getPixel(0,0);
 
       for (var y = 0; y < image1.height(); y++) {
         for (var x = image1.width() - 1; x > 0; x--) {
@@ -17,36 +18,36 @@ lwip.open('img/dude-90.jpg', function(err, image1){
           pixel1 = image1.getPixel(x,y);
           pixel2 = image2.getPixel(x,y);
 
-          if (pixel1.r === pixel2.r &&
-              pixel1.g === pixel2.g &&
-              pixel1.b === pixel2.b) {
-
-             if (PRESERVE_MATCHES) {
+          if (pixel1.r === pixel2.r && pixel1.g === pixel2.g && pixel1.b === pixel2.b) {
+            if (PRESERVE_MATCHES) {
               batch.setPixel(x, y, pixel1);
             }
 
             savedPixel = pixel1;
           } else {
-            // Difference
-            // batch.setPixel(x, y, {
-            //   r: Math.abs(pixel2.r - pixel1.r),
-            //   g: Math.abs(pixel2.g - pixel1.g),
-            //   b: Math.abs(pixel2.b - pixel1.b)
-            // });
-
-            // Average
-            // batch.setPixel(x, y, {
-            //   r: Math.floor((pixel2.r + pixel1.r) / 2),
-            //   g: Math.floor((pixel2.g + pixel1.g) / 2),
-            //   b: Math.floor((pixel2.b + pixel1.b) / 2)
-            // });
-
-            // Solid color
-            batch.setPixel(x, y, 'black');
-
-            // Saved pixel
-            // savedPixel.a = savedPixel.a > 0 ? Math.floor(savedPixel.a - 1) : 0; // Fade out
-            // batch.setPixel(x, y, savedPixel);
+            ({
+              difference: function () {
+                batch.setPixel(x, y, {
+                  r: Math.abs(pixel2.r - pixel1.r),
+                  g: Math.abs(pixel2.g - pixel1.g),
+                  b: Math.abs(pixel2.b - pixel1.b)
+                });
+              },
+              average: function () {
+                batch.setPixel(x, y, {
+                  r: Math.floor((pixel2.r + pixel1.r) / 2),
+                  g: Math.floor((pixel2.g + pixel1.g) / 2),
+                  b: Math.floor((pixel2.b + pixel1.b) / 2)
+                });
+              },
+              solid: function () {
+                batch.setPixel(x, y, 'black');
+              },
+              savedPixel: function () {
+                savedPixel.a = savedPixel.a > 0 ? Math.floor(savedPixel.a - 1) : 0; // Fade out
+                batch.setPixel(x, y, savedPixel);
+              }
+            })[VIZMODE]();
           }
         }
         console.log('Finished row ' + (y + 1) + ' of ' + image1.height());
